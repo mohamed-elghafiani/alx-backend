18d9f1a6cd8dae48b10382255cb504eb136783ae
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-04-09 15:07:13 -0700

    Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Basic Flask app Module
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    """Home page"""
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
