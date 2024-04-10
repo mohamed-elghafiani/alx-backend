2eafb2530c2ae36a258c1aecc51df6d97b597065
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-04-10 11:19:34 -0700

    resolve issue of accessing user infos

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -55,7 +55,7 @@ def get_locale():
     else:
         user = getattr(g, 'user', None)
         if user is not None:
-            return user.locale
+            return user["locale"]
 
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
