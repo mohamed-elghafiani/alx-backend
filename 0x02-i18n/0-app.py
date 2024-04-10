530ec5d26ff26e2b217c9a41dd7a409586ad28f8
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-04-10 10:09:51 -0700

    fixed typo

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 def home():
@@ -13,4 +14,4 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
